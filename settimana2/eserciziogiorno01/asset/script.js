117b9fe02c7263228c333b5f48269a73b50a7163
lucawar <lucaguerra92@yahoo.it>
2023-04-03 16:32:21 +0200

    settimkana 2 esercizio giorno1 javascript

---
+++ settimana2/eserciziogiorno01/asset/script.js
@@ -0,0 +1,6 @@
+document.getElementById('prima riga').innerHTML = 'output scritto da Javascript';
+document.getElementById('seconda riga').innerHTML = 'pantera scritto da javascript';
+
+
+
+document.write('CIAO A TUTTI <h3>');
---
+++ settimana2/eserciziogiorno01/asset/script2.js
@@ -0,0 +1,6 @@
+window.alert('DAJEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
+alert('ECCOCIIIIIIIIIIIIIIIIIIIIIIIIIIIIII');
+
+console.log('amatriciana');
+console.log('carbonara');
+console.log('cacio e pepe');
